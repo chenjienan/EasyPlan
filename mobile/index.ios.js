d23c7f9223933c74df8cc745293c79bdc3ba357f
HuachuanD <huachuandeng@gmail.com>
2016-05-01 16:44:10 -0700

    temp iOS login page?

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -8,34 +8,61 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
+  TextInput,
+  TouchableHighlight,
+
 } from 'react-native';
 
 class mobile extends Component {
+  constructor() {
+    super();
+    this.state={
+      username: "",
+      password: ""
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style= {styles.title}> Easy Planning </Text>
+        <View stype={styles.inputs}>
+          <TextInput
+                style={styles.inputs}
+                onChangeText={(username) => this.setState({username})}
+                value={this.state.username} />
+
+                <TextInput
+                style={styles.inputs}
+                onChangeText={(password) => this.setState({password})}
+                value={this.state.password} 
+                secureTextEntry = {true} />
+
+                <TouchableHighlight
+                  onPress = {this._onPressButton}
+                  style={styles.button}>
+                  <Text style= {styles.text}>
+                  Clear me!
+                  </Text>
+                </TouchableHighlight>
+        </View>
       </View>
     );
+  }
+
+  _onPressButton() {
+    console.log(this);
+    console.log(this.state);
+    this.state.username="";
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'gray',
   },
   welcome: {
     fontSize: 20,
@@ -47,6 +74,31 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    paddingTop: 100,
+    color: 'white',
+  },
+
+  inputs: {
+    width: 250, 
+    height: 40, 
+    marginTop: 20,
+    backgroundColor: 'white', 
+    borderWidth: 0,
+  },
+  button: {
+
+    width: 200,
+    alignItems: 'center', 
+    paddingTop: 10,
+    height: 40,
+  },
+  text: {
+    textAlign: 'center',
+
+  }
 });
 
 AppRegistry.registerComponent('mobile', () => mobile);
